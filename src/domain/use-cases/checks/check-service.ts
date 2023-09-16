baba2e823dd96c0a69a8b6802c040874abcd3cb2
cuter97 <aguscastets@gmail.com>
2023-09-16 14:18:48 -0300

    inject repository in use case

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
     execute(url: string): Promise<boolean>
 }
@@ -8,10 +11,11 @@ type ErrorCallback = (error: string) => void;
 export class CheckService implements CheckServiceUseCase {
 
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
-    ) {}
-    
+    ) { }
+
     public async execute(url: string): Promise<boolean> {
 
         try {
@@ -21,13 +25,19 @@ export class CheckService implements CheckServiceUseCase {
             if (!req.ok)
                 throw new Error(`Error on check service ${url}`);
 
+            const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+
+            this.logRepository.saveLog(log);
             this.successCallback();
             return true;
 
         } catch (error) {
-            
-            console.log(`${error}`);
-            this.errorCallback(`${error}`);
+
+            const errorMessage = `${url} is not ok. ${error}`;
+            const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+
+            this.logRepository.saveLog(log);
+            this.errorCallback(errorMessage);
 
             return false;
 
